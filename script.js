227290a9616572c1629b7a46833e099816211caa
carsonjack25-cmd <carsonjack25@gmail.com>
2026-06-16 21:57:09 -0500

    Update script.js

--- script.js
+++ script.js
@@ -36,11 +36,11 @@ const categories = {
 
 const lastRolledItems = [
     {
-      "catacombs": "",
-      "skill_xp": "",
-      "fishing": "",
-      "slayer": "",
-      "completions": ""
+      "catacombs": "catacombs_xp",
+      "skill_xp": "foraging_xp",
+      "fishing": "treasures_caught",
+      "slayer": "wolf_xp",
+      "completions": "f7_completions"
     },
 ]
 
@@ -68,6 +68,67 @@ for (const roll of lastRolledItems) {
         forceWeight(category, item);
     }
 }
-const highestWeightItems = {};
-document.getElementById("output").textContent =
-    JSON.stringify(categories, null, 2);
+const odds = {
+    catacombs: {
+        'catacombs_xp': 0,
+        'normal_completions': 0,
+        'master_completions': 0,
+        'secrets': 0
+    },
+    skill_xp: {
+        'combat_xp': 0,
+        'mining_xp': 0,
+        'foraging_xp': 0,
+        'taming_xp': 0,
+        'farming_xp': 0,
+    },
+    fishing: {
+        'jawbus_kills': 0,
+        'thunder_kills': 0,
+        'treasures_caught': 0,
+        'fishing_xp': 0,
+        'blobfish_caught': 0
+    },
+    slayer: {
+        'zombie_xp': 0,
+        'spider_xp': 0,
+        'wolf_xp': 0,
+        'enderman_xp': 0,
+        'blaze_xp': 0,
+        'vampire_xp': 0
+    },
+    completions: {
+        'f7_completions': 0,
+        'm7_completions': 0,
+        't5_completions': 0
+    }
+};
+for (const category in categories) {
+    let totalWeight = Object.values(categories[category]).reduce((a, b) => a + b, 0);
+    for (const item in categories[category]) {
+        odds[category][item] = parseFloat(((categories[category][item] / totalWeight) * 100).toFixed(2)) + '%';
+    }
+}
+
+const highestOdds = {
+    catacombs: {item: null, odds: 0},
+    skill_xp: {item: null, odds: 0},
+    fishing: {item: null, odds: 0},
+    slayer: {item: null, odds: 0},
+    completions: {item: null, odds: 0}
+};
+for (const category in odds) {
+    for (const item in odds[category]) {
+        const itemOdds = parseFloat(odds[category][item]);
+        const currentHighestOdds = parseFloat(highestOdds[category].odds);
+
+        if (itemOdds > currentHighestOdds) {
+            highestOdds[category] = {item, odds: itemOdds + '%'};
+        } else if (itemOdds === currentHighestOdds) {
+            highestOdds[category].item += ` / ${item}`;
+        }
+    }
+}
+
+console.log("Odds:", JSON.stringify(odds, null, 2));
+console.log("Prediction:", JSON.stringify(highestOdds, null, 2));
